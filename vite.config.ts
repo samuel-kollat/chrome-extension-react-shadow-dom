0901d6c4ae4928fe46526893a67b6fb2742a8125
Samuel Kollat <samuel.kollat@gmail.com>
2023-09-16 17:55:12 +0200

    create and register manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from './src/static/manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), crx({ manifest })],
+});
